e4827e17ee058a3fb21c07e9bbfec3deb21f3638
Rahul Bhooteshwar <Rahul.Bhooteshwar@mastercard.com>
2019-12-18 15:49:12 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'stencil-angular';
+  items = Array(20);
+}
